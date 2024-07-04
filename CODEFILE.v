97e7dd82366930746aecdc7069e11275b7cf2949
Vikram GS <160568759+panda12384@users.noreply.github.com>
2024-07-04 12:39:17 +0530

    Create CODEFILE.v

---
+++ CODEFILE.v
@@ -0,0 +1,22 @@
+module CLA_4bit (
+    input [3:0] A,
+    input [3:0] B,
+    input Cin,
+    output [3:0] Sum,
+    output Cout
+);
+
+    wire [3:0] G, P; 
+    wire [4:0] C;    
+
+    assign C[0] = Cin;
+    assign G = A & B;
+    assign P = A ^ B;
+    assign C[1] = G[0] | (P[0] & C[0]);
+    assign C[2] = G[1] | (P[1] & G[0]) | (P[1] & P[0] & C[0]);
+    assign C[3] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & C[0]);
+    assign C[4] = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & P[0] & C[0]);
+    assign Sum = P ^ C[3:0];
+    assign Cout = C[4];
+
+endmodule
